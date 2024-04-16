84551e0baf639d7732d959c0cce1ea3955628b3a
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-04-16 09:31:59 +0530

    feat: add stores page

---
+++ src/pages/stores.jsx
@@ -0,0 +1,14 @@
+import Container from "../components/global/container";
+import Heading from "../components/global/heading";
+
+const Stores = () => {
+  return (
+    <div className="relative">
+      <Container>
+        <Heading title="Stores Page" />
+      </Container>
+    </div>
+  );
+};
+
+export default Stores;
