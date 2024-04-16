34a14a79b75df3c4f9feac2d4a45eefc882b139c
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-04-16 09:32:07 +0530

    feat: add sign in page

---
+++ src/pages/auth/sign-in.jsx
@@ -0,0 +1,12 @@
+import Container from "../../components/global/container";
+import Heading from "../../components/global/heading";
+
+const SignIn = () => {
+  return (
+    <Container>
+      <Heading title="SignIn Page" />
+    </Container>
+  );
+};
+
+export default SignIn;
