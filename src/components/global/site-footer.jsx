bb132238797853f6adc7583d012f1ee052657f72
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-04-16 09:30:14 +0530

    feat: add site footer

---
+++ src/components/global/site-footer.jsx
@@ -0,0 +1,11 @@
+const SiteFooter = () => {
+  return (
+    <div className="border-t border-gray-200/20 overflow-hidden">
+      <div className="py-4 container">
+        <span className="text-white text-sm">© 2024 Fudo, Inc.</span>
+      </div>
+    </div>
+  );
+};
+
+export default SiteFooter;
