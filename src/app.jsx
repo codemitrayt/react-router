8690f152ce56af5a989a26d168871bcd3e385359
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-04-16 09:28:36 +0530

    remove: app.jsx file

--- src/app.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <h1 className="text-pink-500 flex items-center justify-center h-screen text-2xl bg-gray-900">
-      Hello
-    </h1>
-  );
-}
-
-export default App;
