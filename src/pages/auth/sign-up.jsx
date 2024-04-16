033a9797d8e5baa75bd11946356bce0901c7b49f
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-04-16 09:32:14 +0530

    feat: add sign up page

---
+++ src/pages/auth/sign-up.jsx
@@ -0,0 +1,12 @@
+import Container from "../../components/global/container";
+import Heading from "../../components/global/heading";
+
+const SignUp = () => {
+  return (
+    <Container>
+      <Heading title="SignUp Page" />
+    </Container>
+  );
+};
+
+export default SignUp;
